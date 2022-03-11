3db6e8ca1c565dc86b463d84ac3afebfd7549001
Aditya Parasaran U <adityau0408@gmail.com>
2022-03-11 17:09:42 +0530

    Changed the files

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
+import os
 from app import app
 from db import db
 
+uri = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://")
+    print(uri)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "aditya"
 db.init_app(app)
 
 
