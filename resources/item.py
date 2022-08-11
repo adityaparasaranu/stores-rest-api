722c975cd23afc0939b852dc6111f029de82f1ae
Aditya Parasaran U <adityau0408@gmail.com>
2022-08-11 19:27:23 +0530

    Final code

--- resources/item.py
+++ resources/item.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
-    # m
+
 
 class Item(Resource):
     # The below adds only valid items that as been mentioned in the `add_argument` func
